d9ec54e1c5d1140b92dd9e2fa791ca946148c786
Yunjidev <florian.vancamp@gmail.com>
2023-11-28 10:44:23 +0100

    essai réglage formulaire

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,11 +1,21 @@
 class Property < ApplicationRecord
   belongs_to :user
-
   validates :title, presence: true
   validates :price, presence: true
   validates :description, presence: true
-  validates :superficie , presence: true
+  validates :superficie, presence: true
   validates :num_rooms, presence: true
-  validates :furnished, presence: true
+  validates :furnished, inclusion: { in: [true, false] }
+  attribute :superficie, :integer
+  attr_accessor :furnished
 
+
+  def num_rooms
+    self[:num_rooms] || 0
+  end
+
+  def category=(value)
+    self[:category] = value
+  end
+  
 end
